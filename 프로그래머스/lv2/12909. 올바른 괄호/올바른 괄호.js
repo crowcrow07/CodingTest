d32f9672690755ad2c66ebbcab4d7cf07f524185
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-09-24 13:59:24 +0900

    [level 2] Title: 올바른 괄호, Time: 6.34 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,28 +1,15 @@
-function solution(s){
-    let answer = true;
-    let stack = []
-    
-    if(s[0] === ')') {
-        return false
-    }
-    if(s[s.length - 1] === '(') {
-        return false
-    }
+function solution(s) {
+    let stack = [];
 
-    for(let i of s) {
-        if(i === '(') {
-            stack.push(i)
-        }
-        else {
-            if(stack.length === 0) {
-                return false
-            }
-            stack.pop()
+    for (let i of s) {
+        if (i === '(') {
+            stack.push(i);
+        } else if (i === ')' && stack.length > 0) {
+            stack.pop();
+        } else {
+            return false;
         }
     }
-    if(stack.length > 0) {
-        answer = false
-    }
 
-    return answer;
+    return stack.length === 0;
 }
