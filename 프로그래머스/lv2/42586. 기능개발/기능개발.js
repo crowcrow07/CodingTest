af74c903a396e5158dec5d5f343b7ef4018ee21c
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-09-25 13:32:04 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -6,15 +6,10 @@ function solution(progresses, speeds) {
         queue.push(Math.ceil((100 - progresses[i]) / speeds[i] )) 
     }
 
-    
-//     queue의 값이 사라질때까지 반복문을 돌려야함
     while(queue.length > 0) {
-//         첫번째 값과 cnt를 설정
         let first = queue[0]
         let cnt = 0
-//         첫번째값을 뺐을때 0이거나 작다면 cnt 증가, shift()
-//         첫번째값을 뺐을때 0보다 크면? 빼둔채로 break;
-//         while문 조건 : 첫번째값이 0보다 크면 계속 반복을 시켜야하나?
+
         while(queue.length > 0 && queue[0] <= first) {
             cnt++
             queue.shift()
