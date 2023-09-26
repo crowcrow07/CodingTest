43a32da0570843c38d9ad837ae132cd60109c0ac
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-09-26 10:18:13 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -2,15 +2,19 @@ function solution(progresses, speeds) {
     let answer = [];
     let queue = []
     
-    for(let i in progresses) {
-        queue.push(Math.ceil((100 - progresses[i]) / speeds[i] )) 
+    for(let i = 0; i < progresses.length; i++) {
+        queue.push(Math.ceil((100 - progresses[i]) / speeds[i]))
     }
-
+    
+//     queue에 있는 게 사라질때까지 반복문을 돌려야함
     while(queue.length > 0) {
+//         카운트를 정의
+//         첫번째값도 정의
         let first = queue[0]
         let cnt = 0
-
-        while(queue.length > 0 && queue[0] <= first) {
+//         비교하는 반복문을 돌려줌
+//         length가 0 이상이고 queue[0]이 first보다 작거나 같으면 cnt 증가시키면서 shift를 해준다.
+        while(queue.length > 0 && queue[0] <= first ) {
             cnt++
             queue.shift()
         }
