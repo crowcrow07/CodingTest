b605daa229037eebaf48c683c572ee9797361408
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-10-30 10:13:41 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,13 +1,8 @@
 function solution(a, b) {
     let answer = 1234567890;
     let leng = a.length
-    let temp = []
-    
-    for(let i = 0; i < leng; i++) {
-        temp.push(a[i] * b[i])
-    }
-    
-    answer = temp.reduce((a, b) => a + b, 0)
+
+    answer = a.reduce((v, _, i) => v += a[i] * b[i], 0)
     
     return answer;
 }
