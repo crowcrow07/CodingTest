b161fe85c66e4f7ddc80dd5b713e241d80964903
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-11-02 11:06:44 +0900

    [level 2] Title: 타겟 넘버, Time: 1.75 ms, Memory: 35.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,20 @@
+function solution(numbers, target) {
+    let answer = 0;
+    let leng = numbers.length;
+    
+    function DFS(L, sum) {
+        if(L === leng) {
+            if(sum === target) {
+                answer++
+            }
+        }
+        else {
+            DFS(L + 1, sum + numbers[L])
+            DFS(L + 1, sum - numbers[L])
+        }
+    }
+    
+    DFS(0, 0)
+    
+    return answer;
+}
