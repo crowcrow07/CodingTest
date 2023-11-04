ba4af432e2c245c4bef05264f03efbe2e1236427
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-11-04 12:54:26 +0900

    [level unrated] Title: 문자열 곱하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
@@ -0,0 +1,14 @@
+function solution(my_string, k) {
+    let answer = '';
+    
+    let arr= [] 
+    
+    while(k !== 0) {
+        k--
+        arr.push(my_string)
+    }
+    
+
+    
+    return answer = arr.join('');
+}
