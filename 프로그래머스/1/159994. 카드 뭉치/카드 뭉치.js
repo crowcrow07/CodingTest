c2ecee513fd6161507707ad626a9839d099f78ca
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-11-15 11:02:26 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,17 @@
+function solution(cards1, cards2, goal) {
+    let answer = '';
+    
+    for(let i of goal) {
+        if(cards1[0] === i) {
+            cards1.shift()
+        }
+        else if(cards2[0] === i) {
+            cards2.shift()
+        }
+        else {
+            return 'No'
+        }
+    }
+    
+    return answer = 'Yes';
+}
