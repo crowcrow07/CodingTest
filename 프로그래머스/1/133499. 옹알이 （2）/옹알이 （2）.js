b24beb64c427cf970a57d818e74ffaeaba0d0a01
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-11-30 11:17:57 +0900

    [level 1] Title: 옹알이 (2), Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,31 @@
+function solution(babbling) {
+    let answer = 0;
+    let duplicationArr = ['ayaaya','yeye', 'woowoo', 'mama']
+    let possibleArr = ['aya', 'ye', 'woo', 'ma']
+    
+    // 연속해서 같은 발음 하는 언어 제거
+    duplicationArr.forEach((v) => {
+        for(let i = 0; i < babbling.length; i++) {
+            if(babbling[i].includes(v)) {
+                babbling.splice(i, 1)
+                i--
+            }
+        }
+    })
+    
+    // 발음 하는 언어 공백으로 변경
+    possibleArr.forEach((v) => {
+        for(let i = 0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replaceAll(v, ' ')
+        }
+    })
+
+    // 공백을 없앴을때 length가 0이면 answer 증가
+   babbling.forEach((_, i) => {
+       babbling[i] = babbling[i].replaceAll(' ', '')
+       if(babbling[i].length === 0) {
+           answer++
+       }
+   })
+    return answer;
+}
