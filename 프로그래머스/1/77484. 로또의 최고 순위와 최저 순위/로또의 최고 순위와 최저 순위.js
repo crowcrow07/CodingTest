0473eb60761577551d7108346ef78b787e514706
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-12-01 11:37:29 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,39 @@
+function solution(lottos, win_nums) {
+    let answer = [];
+    let fullLottoArr = Array.from({length:45}, (_, i) => i++ + 1)
+    
+    // 최저 순위
+    answer.push(lottoGrade(lottos, win_nums))
+    
+    // 0을 -1로 변환
+    lottos = lottos.map((v, i) => {
+        if(v === 0) {
+            return lottos[i] = -1
+        }
+        return v
+    })
+    
+    // 최고 순위
+    answer.unshift(lottoGrade(lottos, win_nums))
+    
+    return answer;
+}
+
+// 로또 순위 책정
+function lottoGrade(arr, win_nums) {
+    let correct = [...win_nums]
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(win_nums.includes(arr[i])) {
+            correct.splice(correct.indexOf(arr[i]), 1)
+        }
+        if(arr[i] === -1) {
+            correct.splice(correct.indexOf(arr[i]), 1)
+        }
+    }
+
+    if(correct.length > 5) {
+        return correct.length
+    }
+    return correct.length + 1
+}
