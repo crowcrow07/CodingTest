59123deffe5f6108672421530360f8e07659a2f9
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2023-12-13 11:42:11 +0900

    [level 3] Title: 양과 늑대, Time: 0.75 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -0,0 +1,26 @@
+function solution(info, edges) {
+    let answer = 0;
+    let graph = Array.from({length: info.length}, () => [])
+    
+    for(let [start, end] of edges) {
+        graph[start].push(end)
+    }
+    
+    function DFS(node, animals, visit) {
+        info[node] === 0 ? animals[0]++ : animals[1]++
+        if(animals[0] <= animals[1]) return;
+        
+        visit.push(...graph[node])
+        visit.forEach((val, idx, arr) => {
+            const rest = arr.filter((_, i) => idx !== i)
+            DFS(val, [...animals], rest)
+        })
+        if(animals[0] > answer) answer = animals[0]
+        return;
+    }
+    
+    
+    DFS(0, [0, 0], [])
+    
+    return answer;
+}
