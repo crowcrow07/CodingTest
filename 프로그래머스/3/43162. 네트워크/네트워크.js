dbbd26d39bf9f2fae72276ad653d9e31ed0bf452
Kim MinJae <88226519+crowcrow07@users.noreply.github.com>
2024-04-22 12:28:26 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -2,20 +2,22 @@ function solution(n, computers) {
     let answer = 0;
     let check = [false]
     
-    function DFS(i) {
+    function getValue(i) {
         check[i] = true
         for(let j = 0; j < computers[i].length; j++) {
             if(computers[i][j] === 1 && !check[j]) {
-                DFS(j)
+                getValue(j)
             }
         }
     }
     
     for(let i = 0; i < computers.length; i++) {
         if(!check[i]) {
-            DFS(i)
+            getValue(i)    
             answer++
         }
+        
     }
+    
     return answer;
 }
